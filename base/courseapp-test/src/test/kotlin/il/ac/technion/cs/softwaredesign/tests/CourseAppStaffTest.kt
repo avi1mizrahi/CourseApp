357a3d1298e233741ce9e9b0fdada556fd911e0f
Avi <avi1mizrahi@gmail.com>
2019-04-30 23:22:14 +0300

    keep staff's test for sanity

--- base/courseapp-test/src/test/kotlin/il/ac/technion/cs/softwaredesign/tests/CourseAppStaffTest.kt
+++ base/courseapp-test/src/test/kotlin/il/ac/technion/cs/softwaredesign/tests/CourseAppStaffTest.kt
@@ -4,11 +4,26 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.present
 import il.ac.technion.cs.softwaredesign.CourseApp
 import il.ac.technion.cs.softwaredesign.CourseAppInitializer
-import org.junit.jupiter.api.Disabled
+import il.ac.technion.cs.softwaredesign.KeyValueStore
+import il.ac.technion.cs.softwaredesign.Storage
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.time.Duration.ofSeconds
 
+
+class MockStorage : Storage {
+    private val encoding = Charsets.UTF_8
+
+    private val keyvalDB = HashMap<String, ByteArray>()
+
+    override fun read(key: ByteArray): ByteArray? {
+        return keyvalDB[key.toString(encoding)]
+    }
+
+    override fun write(key: ByteArray, value: ByteArray) {
+        keyvalDB[key.toString(encoding)] = value
+    }
+}
 
 class CourseAppStaffTest {
     private val courseAppInitializer = CourseAppInitializer()
@@ -17,9 +32,8 @@ class CourseAppStaffTest {
         courseAppInitializer.setup()
     }
 
-    private val courseApp = CourseApp()
+    private val courseApp = CourseApp(KeyValueStore(MockStorage()))
 
-    @Disabled
     @Test
     fun `after login, a user is logged in`() {
         courseApp.login("gal", "hunter2")
@@ -31,7 +45,6 @@ class CourseAppStaffTest {
                 present(isTrue))
     }
 
-    @Disabled
     @Test
     fun `an authentication token is invalidated after logout`() {
         val token = courseApp.login("matan", "s3kr1t")
