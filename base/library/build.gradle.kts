10d928fe9aab44b6543047ac2831ad9ba07ffbf2
Avi <avi1mizrahi@gmail.com>
2019-04-30 18:18:26 +0300

    create empty library test (KeyValueStore)

--- base/library/build.gradle.kts
+++ base/library/build.gradle.kts
@@ -1,3 +1,13 @@
+
+val junitVersion = "5.5.0-M1"
+val hamkrestVersion = "1.7.0.0"
+
 dependencies {
     compile("il.ac.technion.cs.softwaredesign:primitive-storage-layer:1.0")
+
+    testCompile("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testCompile("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+    testCompile("com.natpryce:hamkrest:$hamkrestVersion")
+
+    runtime("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
---
+++ base/library/src/test/kotlin/il/ac/technion/cs/softwaredesign/tests/KeyValueStoreTest.kt
@@ -0,0 +1,22 @@
+package il.ac.technion.cs.softwaredesign.tests
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class KeyValueStoreTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun `TODO`() {
+    }
+
+}
