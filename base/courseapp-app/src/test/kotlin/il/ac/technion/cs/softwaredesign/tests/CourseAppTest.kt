3147ab7e12c0bd97c33e2c143c4c8118bd34b748
Avi Mizrahi <avi1mizrahi@gmail.com>
2019-05-20 14:11:56 +0300

    fix bug in staff's test

--- base/courseapp-app/src/test/kotlin/il/ac/technion/cs/softwaredesign/tests/CourseAppTest.kt
+++ base/courseapp-app/src/test/kotlin/il/ac/technion/cs/softwaredesign/tests/CourseAppTest.kt
@@ -214,7 +214,7 @@ class CourseAppTest {
         courseApp.channelMakeOperator(adminToken, "#test", "matan")
 
         assertThat(runWithTimeout(Duration.ofSeconds(10)) {
-            courseApp.isUserInChannel(adminToken, "#mychannel", "matan")
+            courseApp.isUserInChannel(adminToken, "#test", "matan")
         },
                 isTrue)
     }
