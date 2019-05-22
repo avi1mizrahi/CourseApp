97fccb479e5aee9c2efa2082daee115b00d32dd4
Avi Mizrahi <avi1mizrahi@gmail.com>
2019-05-23 00:50:01 +0300

    return the storage provider (was KV before)

--- base/courseapp-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/CourseAppModule.kt
+++ base/courseapp-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/CourseAppModule.kt
@@ -1,14 +1,20 @@
 package il.ac.technion.cs.softwaredesign
 
 import com.authzee.kotlinguice4.KotlinModule
+import com.google.inject.Provider
 import il.ac.technion.cs.softwaredesign.storage.SecureStorage
 
+class SecureStorageProvider : Provider<SecureStorage> {
+    override fun get(): SecureStorage {
+        return CourseAppImplInitializer.storage
+    }
+}
 
 class CourseAppModule : KotlinModule() {
 
     override fun configure() {
         bind<CourseAppInitializer>().to<CourseAppImplInitializer>()
-        bind<SecureStorage>().toInstance(CourseAppImplInitializer.storage)
+        bind<SecureStorage>().toProvider(SecureStorageProvider())
         bind<KeyValueStore>().to<KeyValueStoreImpl>()
         bind<CourseApp>().to<CourseAppImpl>()
         bind<CourseAppStatistics>().to<CourseAppStatisticsImpl>()
