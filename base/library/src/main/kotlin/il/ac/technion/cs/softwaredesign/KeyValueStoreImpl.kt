26e379a6d53efa62549f5b7084e68c708f24d6c1
AlonTavor <aloni49@gmail.com>
2019-08-21 12:45:30 +0000

    Moved storage cache to the correct layer (KVStore has multiple instances, SyncStorage is a singleton)

--- base/library/src/main/kotlin/il/ac/technion/cs/softwaredesign/KeyValueStoreImpl.kt
+++ base/library/src/main/kotlin/il/ac/technion/cs/softwaredesign/KeyValueStoreImpl.kt
@@ -5,10 +5,6 @@ import il.ac.technion.cs.softwaredesign.storage.SecureStorage
 import kotlinx.serialization.SerialId
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.protobuf.ProtoBuf
-import java.util.*
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.ConcurrentLinkedQueue
-import kotlin.collections.LinkedHashMap
 
 // can by anything, it is only to notify the array isn't empty, for supporting empty array values.
 // the only thing important is to remove these bytes before loading
@@ -26,64 +22,17 @@ class KeyValueStoreImpl @Inject constructor(private val storage: SyncStorage) : 
                                   serializer: Serializer<V>): KeyValueStore.Object<V> =
             Ref(key, serializer)
 
-    private val CACHE_SIZE = 700
-    private val cache = HashMap<ByteArray, ByteArray?>()
-    private val cacheKeys = LinkedList<ByteArray>()
-
     private inner class Ref<V>(private val key: List<String>,
                                private val serializer: Serializer<V>) : KeyValueStore.Object<V> {
 
-        override fun write(value: V) {
-            val k = convertKeyToByteArray(key)
-            val v = serializer.dump(value) + validEntrySuffix
-            storage.write(k, v)
-            updateCache(k, v)
-        }
+        override fun write(value: V) =
+                storage.write(convertKeyToByteArray(key), serializer.dump(value) + validEntrySuffix)
 
-
-        override fun read(): V? {
-            val k = convertKeyToByteArray(key)
-
-            var bytes = readFromCache(k)
-            if (bytes == null) {
-                bytes = storage.read(k)
-                updateCache(k, bytes)
-            }
-
-            return bytes?.takeIf(ByteArray::isNotEmpty)
+        override fun read(): V? =
+                storage.read(convertKeyToByteArray(key))
+                    ?.takeIf(ByteArray::isNotEmpty)
                     ?.let { serializer.load(it.dropLast(1).toByteArray()) }
-        }
 
-        override fun delete() {
-            val k = convertKeyToByteArray(key)
-            storage.write(k, ByteArray(0))
-            updateCache(k, ByteArray(0))
-        }
-    }
-
-
-    @Synchronized
-    private fun readFromCache(key: ByteArray) : ByteArray? {
-        if (cache.contains(key)) {
-            return cache[key]
-        }
-        return null
-    }
-
-    @Synchronized
-    private fun updateCache(key: ByteArray, value: ByteArray?) {
-        if (cache.size >= CACHE_SIZE) {
-            val keyToRemove = cacheKeys.poll()
-            cache.keys.remove(keyToRemove)
-        }
-
-
-        if (cache.containsKey(key)) {
-            cacheKeys.remove(key) // This is O(N)
-        }
-        cacheKeys.add(key)
-
-
-        cache[key] = value ?: ByteArray(0)
+        override fun delete() = storage.write(convertKeyToByteArray(key), ByteArray(0))
     }
 }
--- base/library/src/main/kotlin/il/ac/technion/cs/softwaredesign/TempAdapterStorage.kt
+++ base/library/src/main/kotlin/il/ac/technion/cs/softwaredesign/TempAdapterStorage.kt
@@ -3,6 +3,7 @@ package il.ac.technion.cs.softwaredesign
 import com.google.inject.Inject
 import il.ac.technion.cs.softwaredesign.storage.SecureStorage
 import il.ac.technion.cs.softwaredesign.storage.SecureStorageFactory
+import java.util.*
 import java.util.concurrent.CompletableFuture
 
 /** This is an adapter interface for the previous storage,
@@ -17,11 +18,51 @@ interface SyncStorage {
 class AsyncStorageAdapter @Inject constructor(private val secureStorageFactory: SecureStorageFactory) : SyncStorage {
     val secureStorage = secureStorageFactory.open("main".toByteArray()).join()
 
-    override fun read(key: ByteArray): ByteArray? =
-            secureStorage.read(key).join()
+    override fun read(key: ByteArray): ByteArray? {
+        var bytes = readFromCache(key)
+        if (bytes == null) {
+            bytes = secureStorage.read(key).join()
+            updateCache(key, bytes)
+        }
+        return bytes
+    }
 
-    override fun write(key: ByteArray, value: ByteArray) =
-            secureStorage.write(key, value).join()!!
+
+    override fun write(key: ByteArray, value: ByteArray) {
+        secureStorage.write(key, value).join()!!
+        updateCache(key, value)
+    }
+
+
+
+    private val CACHE_SIZE = 700
+    private val cache = HashMap<ByteArray, ByteArray?>()
+    private val cacheKeys = LinkedList<ByteArray>()
+
+
+    @Synchronized
+    private fun readFromCache(key: ByteArray) : ByteArray? {
+        if (cache.contains(key)) {
+            return cache[key]
+        }
+        return null
+    }
+
+    @Synchronized
+    private fun updateCache(key: ByteArray, value: ByteArray?) {
+        if (cache.size >= CACHE_SIZE) {
+            val keyToRemove = cacheKeys.poll()
+            cache.keys.remove(keyToRemove)
+        }
+
+        if (cache.containsKey(key)) {
+            cacheKeys.remove(key) // This is O(N)
+        }
+        cacheKeys.add(key)
+
+
+        cache[key] = value ?: ByteArray(0)
+    }
 
 }
 
